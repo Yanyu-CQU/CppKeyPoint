d5729338825bd7529c59456085dd1a63765a079c
Yanyu <yanyu695561506@gmail.com>
2018-04-28 17:43:43 +0800

    增加分析虚析构的意义

--- C++类的默认函数.cpp
+++ C++类的默认函数.cpp
@@ -15,17 +15,17 @@ public:
 		m_hour = hour;
 		m_minute = minute;
 		m_second = second;
-		cout << "构造时间类对象中..." << endl;
+		cout << "construct Time with 3 para" << endl;
 	}
 	Time()//无参构造和全缺省构造不能同时存在 
 	{
-		cout << "无参构造" << endl;
+		cout << "construct Time" << endl;
 	}
-	~Time() 
+	virtual ~Time() //虚析构的意义在于，什么类型的对象，调用什么类型的析构，防止指针向上转型之后，调用了父类的析构
 	{
-		cout << "析构函数永远只有一个！" << endl;
+		cout << "distruct Time" << endl;
 	}
-	print()
+	virtual void print()
 	{
 		cout << m_hour << "-" << m_minute << "-" << m_second << endl;
 	}
@@ -34,12 +34,24 @@ public:
 		m_hour = t.m_hour;
 		m_minute = t.m_minute;
 		m_second = t.m_second;
-		cout << "拷贝构造！" << endl;
+		cout << "copy construct Time" << endl;
 	} 
 private:
 	int m_hour;
 	int m_minute;
 	int m_second;
+};
+class TimeSon : public Time
+{
+private:
+	~TimeSon()
+	{
+		cout << "distruct TimeSon" << endl;
+	}
+	void print()
+	{
+		cout << "output from TimeSon print" << endl;	
+	}	
 };
 class Date
 {
@@ -75,10 +87,15 @@ private:
 };
 int main()
 {
-	{ 
+	{
 		Time t(10,36,20);//这个t析构一次 
 		Date d(2016,7,6,t);//这里的t是值拷贝，里面的形参析构一次，然后Date的成员m_t析构一次 
 		d.print();
+		Time *ts = new TimeSon;
+		ts->print();
+		delete ts;
+		//关于析构函数的调用顺序：首先调用子类析构，然后调用父类析构；构造函数是首先调用父类构造，然后是子类构造（盖房子自基到顶，拆房子自顶到基）
+		//如果父类析构没有virtual，子类有，那么delete父类指针只能调用父类析构。
 	}
 	return 0;
 }
